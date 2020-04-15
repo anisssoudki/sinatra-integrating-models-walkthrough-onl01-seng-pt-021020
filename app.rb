2e67f610c6cae92978b5a8ef6d565043e51def87
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-15 19:06:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     @constants = nonvowels
     sameletter = []
     
-     @commonletter = text_from_user.split(" ").join.split("").sort..each_with_object(Hash.new(0)) { |e,h| h[e] +=1 }
+     @commonletter = text_from_user.split(" ").join.split("").sort.each_with_object(Hash.new(0)) { |e,h| h[e] +=1 }
      
         end
           
