e154b493c9674ea4f34f73d4cdc370ba31cf6207
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-15 19:29:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App < Sinatra::Base
     @constants = nonvowels
     sameletter = []
     
-     @commonletter = text_from_user.split(" ").join.split("").each_with_object(Hash.new(0)) { |e,h| h[e] +=1 }
+     hashy = text_from_user.split(" ").join.split("").each_with_object(Hash.new(0)) { |e,h| h[e] +=1 }
      
-       
+       @numrepeat = hashy.values.max
     erb :results
   end
 end
