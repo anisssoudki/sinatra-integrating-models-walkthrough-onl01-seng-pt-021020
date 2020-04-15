052e74f38e0b44865ea88cb5c188edfb7e96000a
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-15 19:34:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,8 +21,7 @@ class App < Sinatra::Base
        @numrepeat = hashy.values.max
        
        @commonletter =  hashy.each do 
-         |k,v|  k 
-         if v == @numrepeat 
+         |k,v|  k if v == @numrepeat 
            
          
        end
