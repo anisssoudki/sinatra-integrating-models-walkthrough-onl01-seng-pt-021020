bb6785596000daf161f1758cc3a1ae1abdbd3164
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-15 19:09:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     
      @commonletter = 
      def difference(other)
-     @text.split(" ").join.split("").sort.each_with_object(Hash.new(0)) { |e,h| h[e] +=1 }
+     Array.split(" ").join.split("").sort.each_with_object(Hash.new(0)) { |e,h| h[e] +=1 }
         end
           difference(@text)
     erb :results
